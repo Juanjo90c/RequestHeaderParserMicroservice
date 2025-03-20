15faf08fce630f8d3a1adf5610e2e1abfc3d1c3c
Juan Cochia <juanjo90c@gmail.com>
2025-03-20 15:35:42 +0000

    first try

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', function (req,res){
+  let ipAdress = req.headers['x-forwarded-for']
+  let language = req.headers['accept-language']
+  let software = req.headers['user-agent']
+  res.json({"ipaddress": ipAdress, "language":language, "software": software})  
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
